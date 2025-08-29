5219f8e069ae8b2f3c80d42620f3deb1f0dfa62c
stsam <stsamudi@gmail.com>
2025-08-29 20:35:29 +0530

    project added

---
+++ src/test/java/lk/acpt/smartbizspring/SmartBizSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.acpt.smartbizspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBizSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
