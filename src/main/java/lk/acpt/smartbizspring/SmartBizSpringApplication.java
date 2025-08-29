affb4036d2f1bf56228c4662cdb4c05abee8204a
stsam <stsamudi@gmail.com>
2025-08-29 20:35:29 +0530

    project added

---
+++ src/main/java/lk/acpt/smartbizspring/SmartBizSpringApplication.java
@@ -0,0 +1,13 @@
+package lk.acpt.smartbizspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBizSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBizSpringApplication.class, args);
+	}
+
+}
